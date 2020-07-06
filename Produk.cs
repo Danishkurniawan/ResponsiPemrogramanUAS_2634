0a4a326f2daf8faf4fea09795fd033fefa0f8179
Danish Kurniawan <61901597+Danishkurniawan@users.noreply.github.com>
2020-07-06 17:21:19 +0700

    Responsi Pemrograman UAS 19.11.2634

---
+++ Produk.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProdukSaya
+{
+    public class Produk
+    {
+        // PERINTAH: lengkapi property class produk, sesuai petunjuk soal
+        public string PG_produk { get; set; }
+        public string Nama_produk { get; set; }
+        public double Harga_beliproduk { get; set; }
+        public double Harga_jualproduk { get; set; }
+    }
+}
